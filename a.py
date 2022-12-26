a5029a2445d9816732e4e1f1a5c19a94dbfe0deb
Curt-Lucas <60093618+Curt-Lucas@users.noreply.github.com>
2022-12-25 20:06:33 -0500

    init

---
+++ a.py
@@ -0,0 +1,52 @@
+import socket
+import threading
+
+connections = []
+total_connections = 0
+
+class Client(threading.Thread):
+    def __init__(self, socket, address, id, name, signal):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.address = address
+        self.id = id
+        self.name = name
+        self.signal = signal
+    
+    def __str__(self):
+        return str(self.id) + " " + str(self.address)
+    
+    def run(self):
+        while self.signal:
+            try:
+                data = self.socket.recv(32)
+            except:
+                print("Client " + str(self.address) + " has disconnected")
+                self.signal = False
+                connections.remove(self)
+                break
+            if data != "":
+                print("ID " + str(self.id) + ": " + str(data.decode("utf-8")))
+                for client in connections:
+                    if client.id != self.id:
+                        client.socket.sendall(data)
+
+def newConnections(socket):
+    while True:
+        sock, address = socket.accept()
+        global total_connections
+        connections.append(Client(sock, address, total_connections, "Name", True))
+        connections[len(connections) - 1].start()
+        print("New connection at ID " + str(connections[len(connections) - 1]))
+        total_connections += 1
+
+def main():
+    host = 'localhost'
+    port = 4488
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, port))
+    sock.listen(5)
+    newConnectionsThread = threading.Thread(target = newConnections, args = (sock,))
+    newConnectionsThread.start()
+    
+main()
